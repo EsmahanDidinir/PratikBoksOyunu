cece611ee97b1a3f0333d224bf29917d552db2db
EsmahanDidinir <101586224+EsmahanDidinir@users.noreply.github.com>
2023-09-27 14:54:05 +0300

    Add files via upload

---
+++ BoksOyunu/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Fighter marc = new Fighter("Marc", 15, 100, 90, 0);
+		Fighter alex = new Fighter("Alex", 10, 95, 100, 80);
+
+		Match match = new Match(marc, alex, 80, 100);
+		match.run();
+
+	}
+
+}
